1cb6b4f03db85f41a97945766a96b6d5565832d7
unknown <you@example.com>
2023-09-07 22:58:15 +0300

    changing the front

--- GUI/window.py
+++ GUI/window.py
@@ -9,6 +9,12 @@ from Replace.Wos import Wos
 
 win = Tk()
 
+#списки со статьями
+list_scopus = []
+list_wos = []
+list_elibrary = []
+list_ipublishing = []
+
 #при закрытии
 def on_closing():
     if messagebox.askokcancel("Выход из приложения", "Хотите выйти из приложения?"):
@@ -17,8 +23,7 @@ def on_closing():
 
 def open_file_Scopus():
     ftypes = [('xls', 'xlsx')]  # допустимые типы
-    dlg = tkinter.filedialog.Open(filetypes=ftypes, title='Выберите файл',
-                                  initialdir=os.path.abspath(__file__))  # окошко открытия файла
+    dlg = tkinter.filedialog.Open(filetypes=ftypes, title='Выберите файл', initialdir=os.path.abspath(__file__))  # окошко открытия файла
     filename = dlg.show()  # получение имени файла для дальнейшей работы
     if len(filename) > 0:  # если не пустое имя файла
         mainmenu.entryconfigure(3, state=NORMAL)  # разблокирование кнопки сравнить данные
@@ -34,7 +39,7 @@ def open_file_WoS():
     if len(filename)> 0: #если не пустое имя файла
         mainmenu.entryconfigure(3, state=NORMAL) #разблокирование кнопки сравнить данные
         #обработка файла, получение списка данных
-        #list_wos=Wos(filename)
+        list_wos=Wos(filename)
         print("Wos успешно загружен и обработан программой")
         
 
@@ -65,7 +70,7 @@ def open_help_window():
     framehelp = Frame(helpwin, bg='#fafafa') #задаем поле
     framehelp.place(relx=0, rely = 0, relwidth = 1, relheight = 1) #размещаем его на весь размер окна
     l1 = Label(framehelp, text = "Нажмите на иконку нужного сайта и загрузите файл\n\n Кнопка обновить данные сверяет данные из файла \nс данными в базе данных и обновляет данные в базе\n\n Все изменения при обновлении в базу данных \nотмечены в таблице в левой части приложения\n\n Кнопка Выгрузить в XML/XLS выгружает данные\n из базы данных в XML/XLS формат\n")
-    l1.pack() #созаем текст и размещаем по центру
+    l1.pack() #создаем текст и размещаем по центру
 
 
 #создание окна
@@ -95,11 +100,12 @@ filemenu_load.add_command(label="Загрузить Elibrary", command = open_fi
 filemenu_load.add_command(label="Загрузить Ipublishing", command = open_file_Ipublishing)
 filemenu.add_cascade(label="Загрузить файл", menu = filemenu_load)
 
+
 filemenu_unload = Menu(filemenu, tearoff=0) #создание поля выгрузка файлов
 filemenu_unload.add_command(label="Выгрузить файл в xlsx")
 filemenu_unload.add_command(label="Выгрузить файл в xml")
-
 filemenu.add_cascade(label="Выгрузить файл", menu = filemenu_unload)
+
 
 helpmenu = Menu(mainmenu, tearoff=0) #создание поля помощь
 helpmenu.add_command(label="Помощь",command=open_help_window)
@@ -107,7 +113,7 @@ helpmenu.add_command(label="Помощь",command=open_help_window)
 mainmenu.add_cascade(label="Файл", menu=filemenu) #создание поля загрузка файлов
 mainmenu.add_cascade(label="Справка", menu=helpmenu) #создание поля справки
 
-mainmenu.add_cascade(label="Сравнить данные", menu=helpmenu, state=DISABLED) #создание кнопки сравнить
+mainmenu.add_cascade(label="Сравнить данные", state=DISABLED) #создание кнопки сравнить
 
 #большой лист для тестов
 lst = [(1,'author', 'name', 2000),(1,'author', 'name', 2000), (1,'author', 'name', 2000),(1,'author', 'name', 2000),(1,'author', 'name', 2000),(1,'author', 'name', 2000),(3,'author', 'name', 2000),(1,'author', 'name', 2000),(1,'author', 'name', 2000),(5,'author', 'name', 2000),(1,'author', 'name', 2000),(1,'author', 'name', 2000),(1,'author', 'name', 2000),(1,'author', 'name', 2000),(1,'author', 'name', 2000),(1,'author', 'name', 2000)]
