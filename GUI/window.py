99253e6d4f5fe864670edebe8e954ac613e654d0
UWFms <alekceygavr01082004@gmail.com>
2023-09-08 19:43:49 +0300

    table edit

--- GUI/window.py
+++ GUI/window.py
@@ -14,6 +14,7 @@ list_scopus = []
 list_wos = []
 list_elibrary = []
 list_ipublishing = []
+lst = []
 
 #при закрытии
 def on_closing():
@@ -24,12 +25,19 @@ def on_closing():
 def open_file_Scopus():
     ftypes = [('xls', 'xlsx')]  # допустимые типы
     dlg = tkinter.filedialog.Open(filetypes=ftypes, title='Выберите файл', initialdir=os.path.abspath(__file__))  # окошко открытия файла
-    filename = dlg.show()  # получение имени файла для дальнейшей работы
-    if len(filename) > 0:  # если не пустое имя файла
+    filename = dlg.show()  #получение имени файла для дальнейшей работы
+    if len(filename) > 0:  #если не пустое имя файла
         mainmenu.entryconfigure(3, state=NORMAL)  # разблокирование кнопки сравнить данные
         #обработка файла, получение списка данных
         list_scopus=Scopus(filename)
         print("Scopus успешно загружен и обработан программой")
+        for i in range(len(list_scopus)):
+            lst.append((list_scopus[i].author, list_scopus[i].title, list_scopus[i].year,
+                        list_scopus[i].link, list_scopus[i].citation))
+
+        for row in lst:  # для каждой строки указываем что нет родителя (обязательная тема чтоб было красиво)
+            table.insert('', tkinter.END,
+                         values=row)  # без неё слева будет большой пропуст т к предуматривается полноценная иерархия
 
 
 def open_file_WoS():
@@ -65,7 +73,6 @@ def open_help_window():
     helpwin.geometry('385x225')  #размер
     helpwin['bg'] = '#fafafa' #цвет
     helpwin.title("Окно помощи") #название
-    helpwin.wm_attributes("-topmost",1) #помещаем поверх других окон
     helpwin.resizable(False, False) #запрещаем менять размер
     framehelp = Frame(helpwin, bg='#fafafa') #задаем поле
     framehelp.place(relx=0, rely = 0, relwidth = 1, relheight = 1) #размещаем его на весь размер окна
@@ -81,10 +88,21 @@ win.protocol("WM_DELETE_WINDOW", on_closing) #событие при закрыт
 win.resizable(False, False)
 
 #блоки основного окна
-frametable = Frame(win, bg = '#ffb370') #блок с таблицей изменений
+frametableleft = Frame(win, bg = '#ffb370') #блок с таблицей изменений
+frametableright = Frame(win, bg = '#ffb370') #блок с таблицей изменений
 
 #расположение блоков, чтоб можно было менять размер окна
-frametable.place(relx=0, rely = 0, relwidth = 1, relheight = 1)
+frametableleft.place(relx=0, rely = 0, relwidth = 0.5, relheight = 1)
+frametableright.place(relx=0.5, rely = 0, relwidth = 0.5, relheight = 1)
+
+#кнопки
+def left_circle():
+    btn_upload_right_WoS = tkinter.Button(frametableleft, text='WoS', command=open_file_WoS)
+    btn_upload_right_WoS.place()
+
+
+btn_upload_right = tkinter.Button(frametableright,text = 'Загрузить файл', command = left_circle)
+btn_upload_right.place(relx=0.42, rely = 0.07, relwidth = 0.16, relheight = 0.07)
 
 #меню
 mainmenu = Menu(win)
@@ -105,30 +123,21 @@ filemenu_unload.add_command(label="Выгрузить файл в xlsx")
 filemenu_unload.add_command(label="Выгрузить файл в xml")
 filemenu.add_cascade(label="Выгрузить файл", menu = filemenu_unload)
 
-
-helpmenu = Menu(mainmenu, tearoff=0) #создание поля помощь
-helpmenu.add_command(label="Помощь",command=open_help_window)
-
 mainmenu.add_cascade(label="Файл", menu=filemenu) #создание поля загрузка файлов
-mainmenu.add_cascade(label="Справка", menu=helpmenu) #создание поля справки
+mainmenu.add_cascade(label="Справка", command = open_help_window) #создание поля справки
 
-mainmenu.add_cascade(label="Сравнить данные", state=DISABLED) #создание кнопки сравнить
 
-#большой лист для тестов
-lst = [(1,'author', 'name', 2000),(1,'author', 'name', 2000), (1,'author', 'name', 2000),(1,'author', 'name', 2000),(1,'author', 'name', 2000),(1,'author', 'name', 2000),(3,'author', 'name', 2000),(1,'author', 'name', 2000),(1,'author', 'name', 2000),(5,'author', 'name', 2000),(1,'author', 'name', 2000),(1,'author', 'name', 2000),(1,'author', 'name', 2000),(1,'author', 'name', 2000),(1,'author', 'name', 2000),(1,'author', 'name', 2000)]
-
-heads = ['id', 'author', 'name', 'year'] #столбики
-table = ttk.Treeview(frametable, show = 'headings') #инициализация таблицы
+heads = ['author', 'title', 'year', 'link', 'citation'] #столбики
+table = ttk.Treeview(frametableleft, show = 'headings') #инициализация таблицы
 table['columns'] = heads #привязка столбцов к таблице
 
-for header in heads: #для каждого столбика выравниваем по центру все ячейки
-    table.heading(header, text = header, anchor = 'center')
-    table.column(header, anchor = 'center')
 
-for row in lst: #для каждой строки указываем что нет родителя (обязательная тема чтоб было красиво)
-    table.insert('', tkinter.END, values = row) #без неё слева будет большой пропуст т к предуматривается полноценная иерархия
+for header in heads:  # для каждого столбика выравниваем по центру все ячейки
+    table.heading(header, text=header, anchor='center')
+    table.column(header, anchor='center')
 
-scroll_pane = ttk.Scrollbar(frametable, command=table.yview)
+
+scroll_pane = ttk.Scrollbar(frametableleft, command=table.yview)
 table.configure(yscrollcommand=scroll_pane.set)
 scroll_pane.pack(side = tkinter.RIGHT,fill= tkinter.Y)
 table.pack(expand = tkinter.YES, fill = tkinter.BOTH) #штука которая увеличивает таблицу в зависимости от кол-ва строк
