f7821e15ad86cc51b29016e26f87488ef8792dc6
EgorRU <sergeyrootuser@gmail.com>
2023-10-21 18:39:17 +0300

    closing the database connection after unloading

--- Database/db.py
+++ Database/db.py
@@ -303,71 +303,79 @@ def create_db():
 
             
 def update_db(list_new, list_ident, list_remove):
-    #поключаемся к базу данных
-    connection = psycopg2.connect(user="postgres",password=f"{PASSWD}",host="127.0.0.1",port="5432",database="scopus")
-    print("[!] Успешно подключились к базе данных Scopus")
-    connection.autocommit = True
-    cursor = connection.cursor()
-    #заносим новые данные в бд
-    for i in range(len(list_new)):
-        article_tuple = (
-            list_new[i].author,
-            list_new[i].title,
-            int(list_new[i].year) if list_new[i].year!="" else 0,
-            list_new[i].doi,
-            list_new[i].link,
-		    list_new[i].volume,
-            list_new[i].issue,
-            list_new[i].start_page,
-            list_new[i].end_page,
-            list_new[i].number_of_pages,
-            list_new[i].number_article, 
-		    list_new[i].access,
-            list_new[i].citation,
-            list_new[i].eid,
-		    list_new[i].id_author,
-            list_new[i].lang,
-            list_new[i].publication_stage,
-            list_new[i].source_name,
-            list_new[i].type_document
-            )
-        try:
-            cursor.execute('INSERT INTO project_schema.scopus values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)', article_tuple)
-        except Exception as e:
-            writeFile("exception", f"{str(e)}")
+    #поключаемся к базе данных
+    try:
+        connection = psycopg2.connect(user="postgres",password=f"{PASSWD}",host="127.0.0.1",port="5432",database="scopus")
+        print("[!] Успешно подключились к базе данных Scopus")
+        connection.autocommit = True
+        cursor = connection.cursor()
+        #заносим новые данные в бд
+        for i in range(len(list_new)):
+            article_tuple = (
+                list_new[i].author,
+                list_new[i].title,
+                int(list_new[i].year) if list_new[i].year!="" else 0,
+                list_new[i].doi,
+                list_new[i].link,
+		        list_new[i].volume,
+                list_new[i].issue,
+                list_new[i].start_page,
+                list_new[i].end_page,
+                list_new[i].number_of_pages,
+                list_new[i].number_article, 
+		        list_new[i].access,
+                list_new[i].citation,
+                list_new[i].eid,
+		        list_new[i].id_author,
+                list_new[i].lang,
+                list_new[i].publication_stage,
+                list_new[i].source_name,
+                list_new[i].type_document
+                )
+            try:
+                cursor.execute('INSERT INTO project_schema.scopus values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)', article_tuple)
+            except Exception as e:
+                writeFile("exception", f"{str(e)}")
             
-    #добавляем одинаковые
-    for i in range(len(list_ident)):
-        article_tuple = (
-            list_ident[i].author,
-            list_ident[i].title,
-            int(list_ident[i].year) if list_ident[i].year!="" else 0,
-            list_ident[i].doi,
-            list_ident[i].link,
-		    list_ident[i].volume,
-            list_ident[i].issue,
-            list_ident[i].start_page,
-            list_ident[i].end_page,
-            list_ident[i].number_of_pages,
-            list_ident[i].number_article, 
-		    list_ident[i].access,
-            list_ident[i].citation,
-            list_ident[i].eid,
-		    list_ident[i].id_author,
-            list_ident[i].lang,
-            list_ident[i].publication_stage,
-            list_ident[i].source_name,
-            list_ident[i].type_document
-            )
-        try:
-            cursor.execute('INSERT INTO project_schema.scopus values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)', article_tuple)
-        except Exception as e:
-            writeFile("exception", f"{str(e)}")
+        #добавляем одинаковые
+        for i in range(len(list_ident)):
+            article_tuple = (
+                list_ident[i].author,
+                list_ident[i].title,
+                int(list_ident[i].year) if list_ident[i].year!="" else 0,
+                list_ident[i].doi,
+                list_ident[i].link,
+		        list_ident[i].volume,
+                list_ident[i].issue,
+                list_ident[i].start_page,
+                list_ident[i].end_page,
+                list_ident[i].number_of_pages,
+                list_ident[i].number_article, 
+		        list_ident[i].access,
+                list_ident[i].citation,
+                list_ident[i].eid,
+		        list_ident[i].id_author,
+                list_ident[i].lang,
+                list_ident[i].publication_stage,
+                list_ident[i].source_name,
+                list_ident[i].type_document
+                )
+            try:
+                cursor.execute('INSERT INTO project_schema.scopus values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)', article_tuple)
+            except Exception as e:
+                writeFile("exception", f"{str(e)}")
         
-    #удаляем старые
-    for i in range(len(list_remove)):
-        article_tuple = (
-            list_remove[i].eid,
-            list_remove[i].id_author
-            )
-        cursor.execute('DELETE from project_schema.scopus where eid=%s and id_author=%s', article_tuple)
+        #удаляем старые
+        for i in range(len(list_remove)):
+            article_tuple = (
+                list_remove[i].eid,
+                list_remove[i].id_author
+                )
+            cursor.execute('DELETE from project_schema.scopus where eid=%s and id_author=%s', article_tuple)
+    except (Exception, Error) as e:
+        writeFile("exception", f"{str(e)}")
+    finally:
+        if connection:
+            print("[!] Успешно отключились от базы данных")
+            cursor.close()
+            connection.close()
